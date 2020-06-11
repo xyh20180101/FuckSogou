2ae1b74e33be40c30d0b5cd71c894893562cd22c
WL <664823818@qq.com>
2020-06-11 18:10:53 +0800

    Add files via upload

---
+++ FuckSogou/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace FuckSogou
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            var thisProc = Process.GetCurrentProcess();
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                return;
+            }
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
